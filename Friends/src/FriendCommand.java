88481af31f883852b9e2e93ed120c26134cfc292
TheLastImperial <imperial435@gmail.com>
2019-10-21 08:14:53 -0600

    Updated regex to accept regex just in the start.

--- Friends/src/FriendCommand.java
+++ Friends/src/FriendCommand.java
@@ -4,21 +4,21 @@ import java.util.regex.Pattern;
 public class FriendCommand {
     public static final String REGEX_NAME = "([a-zA-Z]+\\s*)+,\\s*([a-zA-Z]+\\s*)+,\\s*([MmFf])\\s*,\\s*(\\d{2}/\\d{2}/\\d{4})";
     // Expresion regular del comando para crear unaa relacion con los indices en el arreglo.
-    private static final String REGEX_CF_BY_INDEX = "\\d+\\s*amigo\\s*\\d+";
+    private static final String REGEX_CF_BY_INDEX = "^(\\d+\\s*amigo\\s*\\d+)";
     // Expresion regular del comando para crear una relacion con los atributos del nodo.
-    private static final String REGEX_CF_BY_ATTR = REGEX_NAME + "\\s*amigo\\s*" + REGEX_NAME;
+    private static final String REGEX_CF_BY_ATTR = "^(" + REGEX_NAME + "\\s*amigo\\s*" + REGEX_NAME + ")";
     // Expresion regular que se encarga de eliminar una relacion por su indice.
-    private static final String REGEX_DEL_BY_INDEX = "\\d+\\s*eliminar\\s*\\d+";
+    private static final String REGEX_DEL_BY_INDEX = "^(\\d+\\s*eliminar\\s*\\d+)";
     // Expresion regulaar que se encarga de eliminar una relacion por los atributos del nodo
-    private static final String REGEX_DEL_BY_ATTR = REGEX_NAME + "\\s*eliminar\\s*" + REGEX_NAME;
+    private static final String REGEX_DEL_BY_ATTR = "^(" + REGEX_NAME + "\\s*eliminar\\s*" + REGEX_NAME + ")";
     // Expresion regular para preguntar por una amitad directa usando sus indices.
-    private static final String REGEX_ASK_F_BY_INDEX = "\\d+\\s*amigos\\s*\\d+";
+    private static final String REGEX_ASK_F_BY_INDEX = "^(\\d+\\s*amigos\\s*\\d+)";
     // Expresion regular para preguntar por una amitad directa usando los atributos del amigo.
-    private static final String REGEX_ASK_F_BY_ATTR = REGEX_NAME + "\\s*amigos\\s*" + REGEX_NAME;
+    private static final String REGEX_ASK_F_BY_ATTR = "^(" + REGEX_NAME + "\\s*amigos\\s*" + REGEX_NAME + ")";
     // Expresion regular para preguntar por un nivel por el indice de un amigo.
-    private static final String REGEX_ASK_L_BY_INDEX = "amigos\\s*\\d+\\s+\\d+";
+    private static final String REGEX_ASK_L_BY_INDEX = "^(amigos\\s*\\d+\\s+\\d+)";
     // Expresion regular para preguntar por un nivel por el indice de un amigo.
-    private static final String REGEX_ASK_L_BY_ATTR = "amigos\\s*" + REGEX_NAME + "\\s+\\d+";
+    private static final String REGEX_ASK_L_BY_ATTR = "^(amigos\\s*" + REGEX_NAME + "\\s+\\d+)";
     // Expresion regular para todas las posibles preguntas o compandos.
     public static final String REGEX_INPUT_FILE = REGEX_CF_BY_INDEX + "|" + REGEX_CF_BY_ATTR + "|" + REGEX_DEL_BY_INDEX
             + "|" + REGEX_DEL_BY_ATTR + "|" + REGEX_ASK_F_BY_INDEX + "|" + REGEX_ASK_F_BY_ATTR
