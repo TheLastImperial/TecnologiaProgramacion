50a9ac6f6b88d2286da2edfb1ce4fc986744a38e
TheLastImperial <imperial435@gmail.com>
2020-01-08 13:51:56 -0700

    Hadamard algorithm.

---
+++ Hadamard/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        //int len = readInt();
+        new MainFrame((int)Math.pow(2, 3));
+    }
+    public static int readInt(){
+        Scanner scan = new Scanner(System.in);
+        int res = 0;
+        while(true){
+            try{
+                System.out.print("Favor de ingresar la longitud: ");
+                res = scan.nextInt();
+            }catch(Exception e){
+                System.out.println("Favor de solo ingresar valores numericos.");
+                scan.nextLine();
+                continue;
+            }
+            break;
+        }
+        return res;
+    }
+}
